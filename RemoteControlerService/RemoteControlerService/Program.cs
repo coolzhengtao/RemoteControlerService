76443d9b82a3e48289bdeca633242c2be96c97bd
Neo_Zheng <coolzhengtao@vip.qq.com>
2014-12-30 14:29:11 +0800

    Add solution of project.
    First time add file to project.

---
+++ RemoteControlerService/RemoteControlerService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteControlerService
+{
+    class Program
+    {
+        //This is the entry of service.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
